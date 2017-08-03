dde0f401e392730b04ef464e33faf383690f9973
Yakiv Yusin <yusin.yakiv@gmail.com>
2017-08-03 16:23:00 +0300

    Added Lemmas to Sentence (without exception checking)

--- SimpleNetNlp/Sentence.cs
+++ SimpleNetNlp/Sentence.cs
@@ -1,17 +1,30 @@
-﻿namespace SimpleNetNlp
+﻿using SimpleNetNlp.Extensions;
+using System.Collections.Generic;
+
+namespace SimpleNetNlp
 {
     public class Sentence
     {
         private edu.stanford.nlp.simple.Sentence nlpSentence;
 
-        public Sentence(string text)
+        public Sentence(string text) : this(new edu.stanford.nlp.simple.Sentence(text))
         {
-            nlpSentence = new edu.stanford.nlp.simple.Sentence(text);
         }
 
         internal Sentence(edu.stanford.nlp.simple.Sentence nlpSentence)
         {
             this.nlpSentence = nlpSentence;
+        }
+
+        public IReadOnlyCollection<string> Lemmas
+        {
+            get
+            {
+                return nlpSentence
+                        .lemmas()
+                        .ToList<string>()
+                        .AsReadOnly();    
+            }
         }
     }
 }
